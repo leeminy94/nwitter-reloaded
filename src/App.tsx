058bfdf875a634f036871eacad4edc400d20f499
leeminy94 <leeminy9408@gmail.com>
2024-03-11 14:41:08 +0900

    commit log

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,67 @@
-function App() {
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from "./components/layout";
+import Home from "./routes/home";
+import Profile from "./routes/profile";
+import Login from "./routes/login";
+import CreateAccount from "./routes/create-account";
+import { createGlobalStyle} from "styled-components";
+import reset from "styled-reset";
+import { useEffect, useState } from "react";
+import LoadingScreen from "./components/loading-screen";
 
-  return <></>;
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<Layout />,
+    children: [
+      {
+        path:"",
+        element:<Home />,
+      },
+      {
+        path:"profile",
+        element:<Profile />,
+      },
+    ],
+  },
+  {
+    path:"/login",
+    element:<Login />
+  },
+  {
+    path: "/create-account",
+    element:<CreateAccount />
+  },
+]);
+
+const GlobalStyles = createGlobalStyle`
+  ${reset};
+  *{
+    box-sizing:border-box;
+  }
+  body{
+    background-color:#000;
+    color:#fff;
+    font-family: system-ui; 
+  }
+`;
+
+
+function App() {
+  const [isLoading, setLoading] = useState(true);
+  const init = async () => {
+    // wait for firebase
+   setLoading(false);
+  };
+  useEffect(() => {
+    init();
+  }, []);
+  return (
+    <>
+    <GlobalStyles />
+    {isLoading ? <LoadingScreen /> : <RouterProvider router={router} />}
+    </>
+  );
 }
 
 export default App;
