5973e49df64f615257ce2cb9c4e281afce634b2f
leeminy94 <leeminy9408@gmail.com>
2024-03-26 17:13:11 +0900

    query timeline

---
+++ src/components/tweet.tsx
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+import { ITweet } from "./timeline";
+
+const Wrapper = styled.div`
+    display:grid;
+    grid-template-columns: 5fr 1fr ;
+    padding:20px;
+    border:1px solid rgba(255, 255, 255, .5);
+    border-radius:15px;
+`;
+
+const Column = styled.div``;
+
+const Photo = styled.img`
+    width:100px;
+    height:100px;
+    border-radius:15px;
+`;
+
+const Username = styled.span`
+    font-weight:600;
+    font-size:15px;
+`;
+
+const Payload = styled.p`
+    margin:10px 0px;
+    font-size:18px;
+`; 
+
+export default function Tweet({username, photo, tweet }:ITweet) {
+    return(
+    <Wrapper>
+        <Column>
+            <Username>{username}</Username>
+            <Payload>{tweet}</Payload>
+        </Column>
+        <Column>
+        {Photo ? (
+       
+            <Photo src={photo} />
+   
+        ) : null}
+        </Column>
+    </Wrapper>)
+}
